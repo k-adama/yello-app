821d2fce3c58c0ad4334df3d01b971bce8b06ebb
TRAORE Abdoul-Aziz <112016586+AzizProg@users.noreply.github.com>
2023-03-15 11:20:51 +0000

    redirection

--- lib/menu.dart
+++ lib/menu.dart
@@ -53,8 +53,8 @@ class _MenuAppState extends State<MenuApp> {
     String? nom = preferences.getString('nom') ?? '';
     print(nom);
     if (nom == '') {
-      Navigator.pushReplacementNamed(context, '/menulecon');
-      //Navigator.pushReplacementNamed(context, '/adminlogin');
+      // Navigator.pushReplacementNamed(context, '/menulecon');
+      Navigator.pushReplacementNamed(context, '/adminlogin');
     } else {
       setState(() {
         student = nom;
